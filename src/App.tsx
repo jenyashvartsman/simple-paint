d4242a4710469c4873c4e02abd4b4cf9aed2fd03
Jenya Shvartsman <jenyas@1touch.io>
2026-01-08 16:41:33 -0500

    feat: initialize BrushSettings component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import BrushSettings from './components/BrushSettings';
 import Canvas from './components/Canvas';
 
 function App() {
   return (
-    <>
-      <Canvas />
-    </>
+    <main className="app">
+      <div className="app__canvas">
+        <Canvas />
+      </div>
+
+      <div className="app__brush-settings">
+        <BrushSettings />
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/BrushSettings.tsx
@@ -0,0 +1,7 @@
+import './BrushSettings.css';
+
+const BrushSettings = () => {
+  return <div className="brush-settings">Brush Settings Component</div>;
+};
+
+export default BrushSettings;
